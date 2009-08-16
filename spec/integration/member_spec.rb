4e417f6ad28c93196d144971de4480393a32a71a
Matthew Landauer <matthew@openaustralia.org>
2009-08-16 18:14:38 +1000

    Do the comparisons by converting both representations to hpricot and back to string

--- spec/integration/member_spec.rb
+++ spec/integration/member_spec.rb
@@ -3,18 +3,32 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 require 'hpricot'
 require 'open-uri'
 
-# Before running these test need to make sure that the original openaustralia php code has its
-# database configuration pointing to the openaustralia_test database (which gets loaded with fixtures data here).
+# Monkeypatch ahoy
+# Sort the outputted attributes into alphabetical order so that they can simply be compared
+module Hpricot
+  class STag < BaseEle
+    def attributes_as_html
+      if @raw_attributes
+        @raw_attributes.sort.map do |aname, aval|
+          " #{aname}" +
+            (aval ? "=\"#{aval}\"" : "")
+        end.join
+      end
+    end
+  end
+end
 
 describe "MembersController" do
   fixtures :member
 
   it "should render the representatives page exactly the same as the php version" do
-    expected = Hpricot(open("http://dev.openaustralia.org/mps"))
+    expected = Hpricot(open("http://dev.openaustralia.org/mps")).to_html
     get "/member"
+    result = Hpricot(@response.body).to_html
     # Write out the expected and resulting html
     File.open("expected.html", "w") {|f| f.write(expected) }
-    File.open("result.html", "w") {|f| f.write(@response.body) }
-    @response.body.should == expected.to_s
+    File.open("result.html", "w") {|f| f.write(result) }
+    result.should == expected
   end
 end
+
