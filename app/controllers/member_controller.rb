2069b56e0c2a6a19477bdd1118e1336b5f3b41d6
Matthew Landauer <matthew@openaustralia.org>
2009-08-17 12:02:22 +1000

    Extracted @title and @members to controller

--- app/controllers/member_controller.rb
+++ app/controllers/member_controller.rb
@@ -1,7 +1,11 @@
 class MemberController < ApplicationController
   def representatives
+    @title = "Representatives"
+    @members = Member.find_all_by_house(1)
   end
   
   def senators
+    @title = "All Senators"
+    @members = Member.find_all_by_house(2)
   end
 end
