92c46d971f3a427f713b62aed45a32e4a21ad813
Matthew Landauer <matthew@openaustralia.org>
2009-08-18 11:20:36 +1000

    Refactored testing a little

--- spec/integration/member_spec.rb
+++ spec/integration/member_spec.rb
@@ -21,28 +21,36 @@ end
 describe "MembersController" do
   fixtures :member, :moffice
 
-  def tidy(text, temp_file)
-    File.open(temp_file, "w") {|f| f.write(text) }
-    system("tidy -q -m #{temp_file}")
-    File.read(temp_file)
+  def tidy(text)
+    File.open("temp.html", "w") {|f| f.write(text) }
+    system("tidy -q -m temp.html")
+    r = File.read("temp.html")
+    File.delete("temp.html")
+    r
   end
   
-  def compare_with_php(url)
+  def compare_with_php(url, name)
     # Setting User-Agent so that the php code outputs the default stylesheets
     expected = Hpricot(open("http://dev.openaustralia.org#{url}", "User-Agent" => "Ruby/#{RUBY_VERSION}")).to_html
     get url
     result = Hpricot(@response.body).to_html
 
-    tidy(result, "result.html").should == tidy(expected, "expected.html")
-    ["expected.html", "result.html"].each {|f| File.delete(f)}
+    expected = tidy(expected)
+    result = tidy(result)
+    if result != expected
+      # If failed then write out result for easy comparison
+      File.open("expected_#{name}.html", "w") {|f| f.write(expected)}
+      File.open("result_#{name}.html", "w") {|f| f.write(result)}
+    end
+    result.should == expected
   end
   
   it "should render the representatives page exactly the same as the php version" do
-    compare_with_php("/mps/")
+    compare_with_php("/mps/", "mps")
   end
   
   it "should render the senators page exactly the same as the php version" do
-    compare_with_php("/senators/")
+    compare_with_php("/senators/", "senators")
   end  
 end
 
