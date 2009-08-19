82fad274fa302ac81ddf17f66570f517b6070a6a
Matthew Landauer <matthew@openaustralia.org>
2009-08-19 16:27:32 +1000

    Now using two special date formats

--- config/initializers/time_formats.rb
+++ config/initializers/time_formats.rb
@@ -1,3 +1,5 @@
 # Used for date formatting in OpenAustralia
 # has form: 18 November 2008
 Date::DATE_FORMATS[:simple] = lambda {|date| date.strftime("#{date.day} %B %Y")}
+Date::DATE_FORMATS[:simple_short] = lambda {|date| date.strftime("#{date.day} %b %Y")}
+
