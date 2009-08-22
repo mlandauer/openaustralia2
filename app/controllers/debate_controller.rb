f8fc6b72d4ff304faa92d44161411c383a0163fd
Matthew Landauer <matthew@openaustralia.org>
2009-08-22 13:38:18 +1000

    Using application layout for rendering debate/show

--- app/controllers/debate_controller.rb
+++ app/controllers/debate_controller.rb
@@ -4,6 +4,5 @@ class DebateController < ApplicationController
     @extra_keyword = "Second Reading: 14 May 2009"
     # Hack to deal with not entirely consistent behaviour of "Debates" menu
     @debates_menu_on = true
-    render :layout => false
   end
 end
