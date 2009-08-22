9f586797f9708d55da04e1e5e84f76d68eac2775
Matthew Landauer <matthew@openaustralia.org>
2009-08-22 13:28:17 +1000

    Added workaround for inconsistency of handling of debates menu

--- app/controllers/debate_controller.rb
+++ app/controllers/debate_controller.rb
@@ -2,6 +2,8 @@ class DebateController < ApplicationController
   def show
     @title = "Second Reading: 14 May 2009: House debates"
     @extra_keyword = "Second Reading: 14 May 2009"
+    # Hack to deal with not entirely consistent behaviour of "Debates" menu
+    @debates_menu_on = true
     render :layout => false
   end
 end
