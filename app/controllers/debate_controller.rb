fff76f58f7b84f97647d322066131d3cd01b35f6
Matthew Landauer <matthew@openaustralia.org>
2009-08-26 17:16:54 +1000

    View is more generic

--- app/controllers/debate_controller.rb
+++ app/controllers/debate_controller.rb
@@ -15,10 +15,24 @@ class DebateController < ApplicationController
   def speech
     raise "at the moment this method should only work for one id value" unless params[:id] == "2009-05-14.66.1"
 
+    @date_string = "Thursday, 14 May 2009"
     @title = "I do not propose to long detain the...: 14 May 2009: House debates"
     @extra_keyword = "I do not propose to long detain the...: 14 May 2009"
     @debates_menu_on = true
     
+    @speech_id = params[:id]
+    @speech_anchor = "g66.1"
     @speech_text = Hansard.find_by_gid("uk.org.publicwhip/debate/#{params[:id]}").text_object.display_body
+    @speech_source = 'http://parlinfo.aph.gov.au/parlInfo/search/display/display.w3p;query=Id:chamber/hansardr/2009-05-14/0000'
+    # The id for the whole debate that this speech is part of
+    @debate_id = "2009-05-14.65.2"
+    @debate_title = "Second Reading"
+    @section_title = "Family Assistance and Other Legislation Amendment (2008 Budget and Other Measures) Bill 2009"
+    # The person speaking
+    @member_id = 1
+    @person_id = 10001
+    @member_name = "Tony Abbott"
+    @member_constituency = "Warringah"
+    @member_party = "Liberal Party"
   end
 end
