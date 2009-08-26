70268befe60f47e27de05a147c4f2ec75734ca38
Matthew Landauer <matthew@openaustralia.org>
2009-08-26 16:47:31 +1000

    New method display_body

--- app/models/text_object.rb
+++ app/models/text_object.rb
@@ -1,6 +1,16 @@
+require 'Hpricot'
+
 class TextObject < ActiveRecord::Base
   set_table_name "epobject"
   # Disabling inheritance_column because we are using "type" as a column name
   # TODO: rename "type" column to something else
   self.inheritance_column = nil
+  
+  # Process body so that it can be displayed
+  def display_body
+    doc = Hpricot(body)
+    # Make all links 'nofollow'
+    (doc/'a').each {|a| a["rel"] = "nofollow"}
+    doc.to_s
+  end
 end
--- spec/models/text_object_spec.rb
+++ spec/models/text_object_spec.rb
@@ -1,13 +1,9 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe TextObject do
-  before(:each) do
-    @valid_attributes = {
-      
-    }
-  end
-
-  it "should create a new instance given valid attributes" do
-    TextObject.create!(@valid_attributes)
+  it "should convert links to nofollow" do
+    t = TextObject.new
+    t.should_receive(:body).and_return('<p>Hello, <a href="http://foo">how</a> are you?</p>')
+    t.display_body.should == '<p>Hello, <a href="http://foo" rel="nofollow">how</a> are you?</p>'
   end
 end
