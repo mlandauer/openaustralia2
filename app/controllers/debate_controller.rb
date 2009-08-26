330745e54b54439b85616948385cd6e2c74c8e79
Matthew Landauer <matthew@openaustralia.org>
2009-08-26 16:48:39 +1000

    Insert speech of text from database

--- app/controllers/debate_controller.rb
+++ app/controllers/debate_controller.rb
@@ -13,8 +13,12 @@ class DebateController < ApplicationController
   
   # Display a single speech
   def speech
+    raise "at the moment this method should only work for one id value" unless params[:id] == "2009-05-14.66.1"
+
     @title = "I do not propose to long detain the...: 14 May 2009: House debates"
     @extra_keyword = "I do not propose to long detain the...: 14 May 2009"
     @debates_menu_on = true
+    
+    @speech_text = Hansard.find_by_gid("uk.org.publicwhip/debate/#{params[:id]}").text_object.display_body
   end
 end
