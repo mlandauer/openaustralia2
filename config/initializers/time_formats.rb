4b1c3e21afe466beb8641209253344c6ac019f0b
Matthew Landauer <matthew@openaustralia.org>
2009-08-26 17:28:54 +1000

    Added new date formatter

--- config/initializers/time_formats.rb
+++ config/initializers/time_formats.rb
@@ -1,5 +1,6 @@
 # Used for date formatting in OpenAustralia
 # has form: 18 November 2008
 Date::DATE_FORMATS[:simple] = lambda {|date| date.strftime("#{date.day} %B %Y")}
+Date::DATE_FORMATS[:simple_with_day] = lambda {|date| date.strftime("#{Date::DAYNAMES[date.wday]}, #{date.day} %B %Y")}
 Date::DATE_FORMATS[:simple_short] = lambda {|date| date.strftime("#{date.day} %b %Y")}
 
--- spec/helpers/member_helper_spec.rb
+++ spec/helpers/member_helper_spec.rb
@@ -4,4 +4,8 @@ describe MemberHelper do
   it "should have access to a nice and simple date formatter" do
     Date.new(2008, 11, 5).to_formatted_s(:simple).should == "5 November 2008"
   end
+  
+  it "should display the day of the week as well" do
+    Date.new(2008, 11, 5).to_formatted_s(:simple_with_day).should == "Wednesday, 5 November 2008"    
+  end
 end
