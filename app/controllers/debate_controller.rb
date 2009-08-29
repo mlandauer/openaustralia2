d4af4bb5ea2488fdd678d62f29e1db29306cd443
Matthew Landauer <matthew@openaustralia.org>
2009-08-29 13:10:47 +1000

    Calculate @title and @extra_keyword in every controller action

--- app/controllers/debate_controller.rb
+++ app/controllers/debate_controller.rb
@@ -1,13 +1,19 @@
 class DebateController < ApplicationController
+  include ActionView::Helpers::TextHelper
+  
   def debate
     if params[:d]
       # A single day of debates in the House of Representatives
       @titles = ["14 May 2009", "House debates"]
+      @title = @titles.map{|t| truncate(t, :length => 38)}.join(': ')
+      @extra_keywords = @titles[0..1].map{|t| truncate(t, :length => 38)}.join(': ')
       render :day, :layout => false
     else
       @subsection = Hansard.find_by_id(params[:id])
       @speeches = Hansard.speeches_in_subsection(@subsection)
       @titles = [@subsection.to_s, @subsection.hdate.to_formatted_s(:simple), "House debates"]
+      @title = @titles.map{|t| truncate(t, :length => 38)}.join(': ')
+      @extra_keywords = @titles[0..1].map{|t| truncate(t, :length => 38)}.join(': ')
       # Hack to deal with not entirely consistent behaviour of "Debates" menu
       @debates_menu_on = true
     end
@@ -19,6 +25,8 @@ class DebateController < ApplicationController
     @previous_speech = @speech.previous_speech_within_subsection
     @next_speech = @speech.next_speech_within_subsection
     @titles = [@speech.just_text, @speech.hdate.to_formatted_s(:simple), "House debates"]
+    @title = @titles.map{|t| truncate(t, :length => 38)}.join(': ')
+    @extra_keywords = @titles[0..1].map{|t| truncate(t, :length => 38)}.join(': ')
     # Hack to deal with not entirely consistent behaviour of "Debates" menu
     @debates_menu_on = true
   end
--- app/controllers/member_controller.rb
+++ app/controllers/member_controller.rb
@@ -1,12 +1,17 @@
 class MemberController < ApplicationController
+  include ActionView::Helpers::TextHelper
+  
   def representatives
-    
     @titles = ["Representatives"]
+    @title = @titles.map{|t| truncate(t, :length => 38)}.join(': ')
+    @extra_keywords = @titles[0..1].map{|t| truncate(t, :length => 38)}.join(': ')
     @members = Member.find_all_by_house(1)
   end
   
   def senators
     @titles = ["All Senators"]
+    @title = @titles.map{|t| truncate(t, :length => 38)}.join(': ')
+    @extra_keywords = @titles[0..1].map{|t| truncate(t, :length => 38)}.join(': ')
     @members = Member.find_all_by_house(2)
   end
 
@@ -15,6 +20,8 @@ class MemberController < ApplicationController
       @member = Member.first(:conditions => {:first_name => $~[1], :last_name => $~[2],
         :constituency => params[:constituency], :house => params[:house]})
       @titles = ["#{@member.formal_name}, #{@member.constituency}"]
+      @title = @titles.map{|t| truncate(t, :length => 38)}.join(': ')
+      @extra_keywords = @titles[0..1].map{|t| truncate(t, :length => 38)}.join(': ')
     else
       # TODO: Add error handling here
     end
