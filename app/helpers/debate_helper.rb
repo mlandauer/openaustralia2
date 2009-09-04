563b24db5f7a900785c30a8dba7268c43568debb
Matthew Landauer <matthew@openaustralia.org>
2009-09-04 14:46:01 +1000

    First pass at view helper for making a little calendar table

--- app/helpers/debate_helper.rb
+++ app/helpers/debate_helper.rb
@@ -1,2 +1,34 @@
 module DebateHelper
+  def calendar(year, month)
+    b = Builder::XmlMarkup.new
+    b.table do
+      b.caption "#{Date::MONTHNAMES[month]} #{year}"
+      b.thead do
+        b.tr do
+          (1..7).each do |day|
+            b.th Date::ABBR_DAYNAMES[day.modulo(7)]
+          end
+        end
+      end
+      b.tbody do
+        # The day of the week for the first day of the month (Monday is 0)
+        day_first_of_month = (Date.new(year, month, 1).wday - 1).modulo(7)
+        b.tr do
+          b.td({:colspan => day_first_of_month}, "&nbsp;") unless day_first_of_month == 0
+          (1..(7-day_first_of_month)).each do |day|
+            b.td day
+          end
+        end
+        current_day = 8 - day_first_of_month
+        while Date.valid_date?(year, month, current_day) do
+          b.tr do
+            (current_day..(current_day + 6)).each do |day|
+              b.td day if Date.valid_date?(year, month, day)
+            end
+          end
+          current_day += 7
+        end
+      end
+    end
+  end
 end
--- spec/helpers/debate_helper_spec.rb
+++ spec/helpers/debate_helper_spec.rb
@@ -1,11 +1,40 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe DebateHelper do
-
-  #Delete this example and add some real ones or delete this file
-  it "should be included in the object returned by #helper" do
-    included_modules = (class << helper; self; end).send :included_modules
-    included_modules.should include(DebateHelper)
+  it "should render a cute little calendar for the month" do
+    # Calendar for January 2009
+    b = Builder::XmlMarkup.new
+    helper.calendar(2009, 1).should == b.table do
+      b.caption 'January 2009'
+      b.thead do
+        b.tr do
+          b.th "Mon"
+          b.th "Tue"
+          b.th "Wed"
+          b.th "Thu"
+          b.th "Fri"
+          b.th "Sat"
+          b.th "Sun"
+        end
+      end
+      b.tbody do
+        b.tr do
+          b.td({:colspan => "3"}, "&nbsp;")
+          (1..4).each {|day| b.td day}
+        end
+        b.tr do
+          (5..11).each {|day| b.td day}
+        end
+        b.tr do
+          (12..18).each {|day| b.td day}
+        end
+        b.tr do
+          (19..25).each {|day| b.td day}
+        end
+        b.tr do
+          (26..31).each {|day| b.td day}
+        end
+      end
+    end
   end
-
 end
