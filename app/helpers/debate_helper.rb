b10ee87da00d13ee3c1edb4ada95501062fe1a88
Matthew Landauer <matthew@openaustralia.org>
2009-09-07 12:51:04 +1000

    Put div tag inside calendar helper

--- app/helpers/debate_helper.rb
+++ app/helpers/debate_helper.rb
@@ -1,54 +1,57 @@
 module DebateHelper
   def calendar(year, month, current_date, recess, linked)
     b = Builder::XmlMarkup.new
-    b.table(:border => 0) do
-      b.caption "#{Date::MONTHNAMES[month]} #{year}"
-      b.thead do
-        b.tr do
-          (1..7).each do |day|
-            b.th Date::ABBR_DAYNAMES[day.modulo(7)]
+    b.div(:class => "calendar") do
+      b.table(:border => 0) do
+        b.caption "#{Date::MONTHNAMES[month]} #{year}"
+        b.thead do
+          b.tr do
+            (1..7).each do |day|
+              b.th Date::ABBR_DAYNAMES[day.modulo(7)]
+            end
           end
         end
-      end
-      b.tbody do
-        # The day of the week for the first day of the month (Monday is 0)
-        day_first_of_month = (Date.new(year, month, 1).wday - 1).modulo(7)
-        if month == 12
-          no_days_in_month = (Date.new(year + 1, 1, 1) - 1).mday
-        else
-          no_days_in_month = (Date.new(year, month + 1, 1) - 1).mday
-        end
-        current_day = 1 - day_first_of_month
+        b.tbody do
+          # The day of the week for the first day of the month (Monday is 0)
+          day_first_of_month = (Date.new(year, month, 1).wday - 1).modulo(7)
+          if month == 12
+            no_days_in_month = (Date.new(year + 1, 1, 1) - 1).mday
+          else
+            no_days_in_month = (Date.new(year, month + 1, 1) - 1).mday
+          end
+          current_day = 1 - day_first_of_month
 
-        while current_day <= no_days_in_month do
-          b.tr do
-            if current_day < 1
-              b.td({:colspan => day_first_of_month}) { b << "&nbsp;" }
-            end
-            (current_day..(current_day + 6)).each do |day|
-              if day > 0 && Date.valid_date?(year, month, day)
-                date = Date.new(year, month, day)
-                if date == current_date
-                  atts = {:class => "on"}
-                elsif recess.find {|r| r.include?(date)}
-                  atts = {:class => "no", :title => "recess"}
-                else
-                  atts = {}
-                end
-                if linked.include?(date)
-                  b.td(atts) { b.a({:href => "/debates/?d=#{date}"}, day) }
-                else
-                  b.td(atts, day)
+          while current_day <= no_days_in_month do
+            b.tr do
+              if current_day < 1
+                b.td({:colspan => day_first_of_month}) { b << "&nbsp;" }
+              end
+              (current_day..(current_day + 6)).each do |day|
+                if day > 0 && Date.valid_date?(year, month, day)
+                  date = Date.new(year, month, day)
+                  if date == current_date
+                    atts = {:class => "on"}
+                  elsif recess.find {|r| r.include?(date)}
+                    atts = {:class => "no", :title => "recess"}
+                  else
+                    atts = {}
+                  end
+                  if linked.include?(date)
+                    b.td(atts) { b.a({:href => "/debates/?d=#{date}"}, day) }
+                  else
+                    b.td(atts, day)
+                  end
                 end
               end
+              if current_day > 0 && !Date.valid_date?(year, month, current_day + 6)
+                b.td(:colspan => (6 - no_days_in_month + current_day)) { b << "&nbsp;" }
+              end
             end
-            if current_day > 0 && !Date.valid_date?(year, month, current_day + 6)
-              b.td(:colspan => (6 - no_days_in_month + current_day)) { b << "&nbsp;" }
-            end
+            current_day += 7
           end
-          current_day += 7
         end
       end
     end
+    b << "<!-- end calendar -->"
   end
 end
--- spec/helpers/debate_helper_spec.rb
+++ spec/helpers/debate_helper_spec.rb
@@ -8,45 +8,51 @@ describe DebateHelper do
     recess = [(Date.new(2009,1,1)..Date.new(2009,1,3)), (Date.new(2009,1,20)..Date.new(2009,1,22))]
     # Dates that should link off to individual days of debates
     linked = [Date.new(2009,1,12), Date.new(2009,1,31)]
-    helper.calendar(2009, 1, Date.new(2009,1,14), recess, linked).should == b.table(:border => 0) do
-      b.caption 'January 2009'
-      b.thead do
-        b.tr do
-          b.th "Mon"
-          b.th "Tue"
-          b.th "Wed"
-          b.th "Thu"
-          b.th "Fri"
-          b.th "Sat"
-          b.th "Sun"
+
+    b.div(:class => "calendar") do
+      b.table(:border => 0) do
+        b.caption 'January 2009'
+        b.thead do
+          b.tr do
+            b.th "Mon"
+            b.th "Tue"
+            b.th "Wed"
+            b.th "Thu"
+            b.th "Fri"
+            b.th "Sat"
+            b.th "Sun"
+          end
         end
-      end
-      b.tbody do
-        b.tr do
-          b.td({:colspan => "3"}) { b << "&nbsp;" }
-          (1..3).each {|day| b.td({:class => "no", :title => "recess"}, day)}
-          b.td 4
-        end
-        b.tr do
-          (5..11).each {|day| b.td day}
-        end
-        b.tr do
-          b.td { b.a({:href => "/debates/?d=2009-01-12"}, 12) }
-          b.td 13
-          b.td({:class => "on"}, 14)
-          (15..18).each {|day| b.td day}
-        end
-        b.tr do
-          b.td 19
-          (20..22).each {|day| b.td({:class => "no", :title => "recess"}, day)}
-          (23..25).each {|day| b.td day}
-        end
-        b.tr do
-          (26..30).each {|day| b.td day}
-          b.td { b.a({:href => "/debates/?d=2009-01-31"}, 31) }
-          b.td({:colspan => 1}) { b << "&nbsp;" }
+        b.tbody do
+          b.tr do
+            b.td({:colspan => "3"}) { b << "&nbsp;" }
+            (1..3).each {|day| b.td({:class => "no", :title => "recess"}, day)}
+            b.td 4
+          end
+          b.tr do
+            (5..11).each {|day| b.td day}
+          end
+          b.tr do
+            b.td { b.a({:href => "/debates/?d=2009-01-12"}, 12) }
+            b.td 13
+            b.td({:class => "on"}, 14)
+            (15..18).each {|day| b.td day}
+          end
+          b.tr do
+            b.td 19
+            (20..22).each {|day| b.td({:class => "no", :title => "recess"}, day)}
+            (23..25).each {|day| b.td day}
+          end
+          b.tr do
+            (26..30).each {|day| b.td day}
+            b.td { b.a({:href => "/debates/?d=2009-01-31"}, 31) }
+            b.td({:colspan => 1}) { b << "&nbsp;" }
+          end
         end
       end
     end
+    b.comment! "end calendar"
+    
+    helper.calendar(2009, 1, Date.new(2009,1,14), recess, linked).should == b
   end
 end
