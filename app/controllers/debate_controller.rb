3cd28457a83e7e749e403affdf405741a02eb1e2
Matthew Landauer <matthew@openaustralia.org>
2009-09-08 15:50:08 +1000

    Using shorter version of method name

--- app/controllers/debate_controller.rb
+++ app/controllers/debate_controller.rb
@@ -18,7 +18,7 @@ class DebateController < ApplicationController
     if params[:d]
       # A single day of debates in the House of Representatives
       @date = Date.parse(params[:d])
-      @extra_keywords = @date.to_formatted_s(:simple)
+      @extra_keywords = @date.to_s(:simple)
       @title = "#{@extra_keywords}: House debates"
       # TODO: There should only be a previous and next date if there are debates on that day
       if @date == Date.new(2009,5,13)
@@ -38,7 +38,7 @@ class DebateController < ApplicationController
       @section = @subsection.section
       @date = @subsection.hdate
       @speeches = Hansard.speeches_in_subsection(@subsection)
-      @extra_keywords = "#{truncate(@subsection.to_s, :length => 38)}: #{@subsection.hdate.to_formatted_s(:simple)}"
+      @extra_keywords = "#{truncate(@subsection.to_s, :length => 38)}: #{@subsection.hdate.to_s(:simple)}"
       @title = "#{@extra_keywords}: House debates"
       @previous_debate = Hansard.find_by_id("2009-05-14.1.1")
     end
@@ -49,7 +49,7 @@ class DebateController < ApplicationController
     @speech = Hansard.find_by_id(params[:id])
     @previous_speech = @speech.previous_speech_within_subsection
     @next_speech = @speech.next_speech_within_subsection
-    @extra_keywords = "#{truncate(@speech.just_text, :length => 38)}: #{@speech.hdate.to_formatted_s(:simple)}"
+    @extra_keywords = "#{truncate(@speech.just_text, :length => 38)}: #{@speech.hdate.to_s(:simple)}"
     @title = "#{@extra_keywords}: House debates"
     # Hack to deal with not entirely consistent behaviour of "Debates" menu
     @debates_menu_on = true
