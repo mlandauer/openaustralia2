a04211990c52c235df8e0229b7317b2b11ded861
Matthew Landauer <matthew@openaustralia.org>
2009-09-14 14:35:51 +1000

    Merged month template and action into index

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -1,17 +1,25 @@
 class NewsController < ApplicationController
   def index
-    @title = "OpenAustralia news"
-    @extra_keywords = @title
     @rss = true
-    
-    @posts = News.all
-    
-    # Figure out the months that posts appear in
-    @months = @posts.map{|p| [p.timestamp.year, p.timestamp.month]}.uniq.map{|a| Date.new(a[0],a[1],1)}.sort.reverse
-    
+    @news_menu_on = true
+    @months = News.all.map{|p| [p.timestamp.year, p.timestamp.month]}.uniq.map{|a| Date.new(a[0],a[1],1)}.sort.reverse
     # HACK: Temporary
     @months << Date.new(2008,6,1)
-    @news_menu_on = true
+
+    if params[:year] && params[:month]
+      @subtitle = "May 2009"
+      @extra_keywords = @subtitle
+      @title = "#{@extra_keywords}: OpenAustralia news"
+
+      posts = News.all
+      @posts = [posts[1], posts[0]]
+    else
+      @title = "OpenAustralia news"
+      @subtitle = "&nbsp;"
+      @extra_keywords = @title
+    
+      @posts = News.all
+    end
   end
   
   def show
@@ -25,18 +33,5 @@ class NewsController < ApplicationController
     @post = posts.find {|p| params[:year] == p.year_param && params[:month] == p.month_param && params[:day] == p.day_param && params[:title] == p.title_param }
     @extra_keywords = @post.title
     @title = "#{@extra_keywords}: OpenAustralia news"
-  end
-  
-  def month
-    @extra_keywords = "May 2009"
-    @title = "#{@extra_keywords}: OpenAustralia news"
-    @rss = true
-    @news_menu_on = true
-    @months = News.all.map{|p| [p.timestamp.year, p.timestamp.month]}.uniq.map{|a| Date.new(a[0],a[1],1)}.sort.reverse
-    # HACK: Temporary
-    @months << Date.new(2008,6,1)
-    
-    posts = News.all
-    @posts = [posts[1], posts[0]]
-  end
+  end  
 end
