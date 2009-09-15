7a8551a23d1936ff819c1a71c214b6f88e267cc1
Matthew Landauer <matthew@openaustralia.org>
2009-09-15 12:40:20 +1000

    New helper which overrides the default time_ago_in_words helper to give the same results as those found in the theyworkforyou code

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -1,2 +1,36 @@
 module CommentsHelper
+  # Override implementation provided by rails to match the results of the php implementation
+  def time_ago_in_words(time)
+    d = Time.now - time
+    months = (d / 4.weeks).floor
+    d -= months * 4.weeks
+    weeks = (d / 1.week).floor
+    d -= weeks.weeks
+    days = (d / 1.day).floor
+    d -= days.days
+    hours = (d / 1.hour).floor
+    d -= hours.hours
+    minutes = (d / 1.minute).floor
+    d -= minutes.minutes
+    seconds = (d / 1.second).floor
+    
+    if months > 0
+      text = time.to_date.to_s(:simple)
+    elsif weeks > 0
+      text = pluralize(weeks, 'weeks')
+      text += ", " + pluralize(days, 'day') if days > 0
+    elsif days > 0
+      text = pluralize(days, 'day')
+      text += ", #{pluralize(hours, 'hour')}" if hours > 0
+    elsif hours > 0
+      text = pluralize(hours, 'hour')
+      text += ", #{pluralize(minutes, 'minute')}" if minutes > 0
+    elsif minutes > 0
+      text = pluralize(minutes, 'minute')
+      text += ", #{pluralize(seconds, 'second')}" if seconds > 0
+    else
+      text = pluralize(seconds, 'second')
+    end
+    text
+  end
 end
--- spec/helpers/comments_helper_spec.rb
+++ spec/helpers/comments_helper_spec.rb
@@ -1,11 +1,40 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe CommentsHelper do
-
-  #Delete this example and add some real ones or delete this file
-  it "should be included in the object returned by #helper" do
-    included_modules = (class << helper; self; end).send :included_modules
-    included_modules.should include(CommentsHelper)
+  include CommentsHelper
+  
+  it "should display days and hours" do
+    time_ago_in_words((3.days + 18.hours).ago).should == "3 days, 18 hours"
   end
-
+  
+  it "should handle singular/plural" do
+    time_ago_in_words((1.day + 1.hour).ago).should == "1 day, 1 hour"    
+  end
+  
+  it "should not display hours when exact number of days" do
+    time_ago_in_words(6.days.ago).should == "6 days"
+  end
+  
+  it "should display weeks when appropriate" do
+    time_ago_in_words((3.weeks + 3.days).ago).should == "3 weeks, 3 days"
+  end
+  
+  it "should display hours and minutes" do
+    time_ago_in_words((5.hours + 1.minute).ago).should == "5 hours, 1 minute"
+  end
+  
+  it "should display minutes and seconds" do
+    time_ago_in_words((5.minutes + 1.second).ago).should == "5 minutes, 1 second"
+  end
+  
+  it "should display only seconds" do
+    time_ago_in_words(30.seconds.ago).should == "30 seconds"
+  end
+  
+  it "should just display the date if it's four weeks or longer ago" do
+    Time.stub!(:now).and_return(Time.parse("Tue 15 Sep 2009 12:23:54 EST"))
+    time_ago_in_words((3.weeks + 6.days).ago).should == "3 weeks, 6 days"
+    time_ago_in_words(4.weeks.ago).should == "18 August 2009"
+    time_ago_in_words((4.weeks + 1.day).ago).should == "17 August 2009"
+  end
 end
