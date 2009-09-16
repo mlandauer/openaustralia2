c880b5d8acd1283ff1495bd3eba2ecfb08af25af
Matthew Landauer <matthew@openaustralia.org>
2009-09-16 15:45:04 +1000

    Comments on home page are now calculated

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,8 @@ class HomeController < ApplicationController
   def index
     @title = "OpenAustralia.org: Are your Representatives and Senators working for you in Australia's Parliament?"
     @rss = true
+    
+    # TODO: There will be some limit on the number of comments shown here
+    @comments = Comment.all(:order => "posted DESC")
   end
 end
