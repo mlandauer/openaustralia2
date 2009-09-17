11143ecfe36ea69d14ab4d5f68a86e1d1fc8b509
Matthew Landauer <matthew@openaustralia.org>
2009-09-17 21:34:45 +1000

    Beginnings of new truncate helper with added goodness

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,5 +10,30 @@ module ApplicationHelper
   def escape_path(text)
     text.gsub('/','%2F').gsub('?', '%3F').gsub('=', '%3D').gsub('#', '%23')
   end
-end
+  
+  def truncate(text, *args)
+    options = args.extract_options!
+    unless args.empty?
+      ActiveSupport::Deprecation.warn('truncate takes an option hash instead of separate ' +
+        'length and omission arguments', caller)
 
+      options[:length] = args[0] || 30
+      options[:omission] = args[1] || "..."
+    end
+    options.reverse_merge!(:length => 30, :omission => "...", :break_on_spaces => false)
+
+    if text
+      l = options[:length] - options[:omission].mb_chars.length
+      chars = text.mb_chars
+      if chars.length > options[:length]
+        chopped = chars[0...l]
+        if options[:break_on_spaces]
+          chopped = chopped.match(/(.*)\s.+/)[1]
+        end
+        (chopped + options[:omission]).to_s
+      else
+        text.to_s
+      end
+    end
+  end
+end
---
+++ spec/helpers/application_helper_spec.rb
@@ -0,0 +1,17 @@
+require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+
+describe ApplicationHelper do
+  include ApplicationHelper
+  
+  it "should not truncate text that is too short" do
+    truncate("Hello, how are you?", :length => 30).should == "Hello, how are you?"
+  end
+  
+  it "should truncate text that is too long" do
+    truncate("Hello, how are you?", :length => 10).should == "Hello, ..."
+  end
+  
+  it "should truncate text only on spaces between words" do
+    truncate("leave comments, post links.", :length => 20, :break_on_spaces => true).should == "leave comments,..."
+  end
+end
